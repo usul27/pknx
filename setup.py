1617f3074bab3915100333d8877104e46ceaa48d
Daniel Matuschek <daniel@hifiberry.com>
2016-06-21 21:03:53 +0200

    Push to version 0.2.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='knxip',
-      version='0.2.1',
+      version='0.2.2',
       description='A native Python KNX/IPNet implementation',
       url='http://github.com/usul27/pknx',
       author='Daniel Matuschek',
