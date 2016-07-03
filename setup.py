ead46bafcb692c8684a6848ba2dc668e1ca3ffd2
Daniel Matuschek <daniel@hifiberry.com>
2016-07-03 14:42:06 +0200

    Added more setup options to becomd pip compliant

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='knxip',
       version='0.2.2',
       description='A native Python KNX/IPNet implementation',
       url='http://github.com/usul27/pknx',
       author='Daniel Matuschek',
-      author_email='daniel@hifiberry.com',
+      author_email='daniel@matuschek.net',
       license='MIT',
-      packages=['knxip'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+        ],
+      packages=find_packages(),
+      keywords = 'knx ip automation',
       zip_safe=False)
