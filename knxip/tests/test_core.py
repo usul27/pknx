44f530e22011d249b5c9d543dc7301f93ead1219
Daniel Matuschek <daniel@hifiberry.com>
2017-07-02 08:55:45 +0200

    Bugfix in test: 2-layer group addresses were handled incorrectly

--- knxip/tests/test_core.py
+++ knxip/tests/test_core.py
@@ -9,8 +9,8 @@ class KNXIPCoreTestCase(unittest.TestCase):
         """Does the group address parser work correctly?"""
         self.assertEquals(parse_group_address("1"), 1)
         self.assertEquals(parse_group_address("1678"), 1678)
-        self.assertEquals(parse_group_address("1/1"), 257)
-        self.assertEquals(parse_group_address("2/2"), 514)
+        self.assertEquals(parse_group_address("1/1"), 2049)
+        self.assertEquals(parse_group_address("2/2"), 4098)
         self.assertEquals(parse_group_address("0/0/1"), 1)
         self.assertEquals(parse_group_address("1/1/1"), 2305)
         self.assertEquals(parse_group_address("4/8/45"), 10285)
